325d06edc089fdb98e82d9a928c6d9060c79978a
Partokaris <patrickkaristhairu@gmail.com>
2023-04-13 17:20:27 +0300

    add some files
    theme pics and some other

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def Homepage():
-    return render_template("homepage.html")
+    return render_template("homepage.html",title="Home Page")
 
 if __name__=="__main__":
     app.run(debug=True)
