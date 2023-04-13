1515809a9ac58cae8d689c2d5851b3dcb9add00b
Partokaris <patrickkaristhairu@gmail.com>
2023-04-13 15:25:40 +0300

    added html and css
    please check

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
+from flask import Flask , render_template
 from flask import Flask
 
 app=Flask(__name__)
-
 @app.route("/")
 def Homepage():
-    return "hello patri c k"
+    return render_template("homepage.html")
+
 if __name__=="__main__":
     app.run(debug=True)
